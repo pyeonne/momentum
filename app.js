4c868c16eaae086f522332924f3b51c103ee0d2a
pyeonne <pyeonne@gmail.com>
2021-08-15 22:57:57 +0900

    📝 Add returns example

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 'use strict';
 
 const calculator = {
-    add: function (a, b) {
-        console.log(a + b);
+    plus: function (a, b) {
+        return a + b;
     },
     minus: function (a, b) {
-        console.log(a - b);
+        return a - b;
+    },
+    times: function (a, b) {
+        return a * b;
     },
     divide: function (a, b) {
-        console.log(a / b);
+        return a / b;
     },
-    multiple: function (a, b) {
-        console.log(a * b);
-    },
-    powerOf: function (a, b) {
-        console.log(a ** b);
+    power: function (a, b) {
+        return a ** b;
     },
 };
 
-calculator.add(2, 5);
-calculator.minus(2, 5);
-calculator.divide(2, 5);
-calculator.multiple(2, 5);
-calculator.powerOf(2, 5);
+const plusResult = calculator.plus(2, 3);
+const minusResult = calaulator.minus(plusResult, 10);
+const timesResult = calculator.times(10, minusResult);
+const divideResult = calculator.divide(timesResult, plusResult);
+const powerResult = calculator.power(divideResult, minusResult);
