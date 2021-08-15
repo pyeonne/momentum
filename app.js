93c6ea9dfb650a26632be9ddf66a7e478e8873e6
pyeonne <pyeonne@gmail.com>
2021-08-15 14:34:21 +0900

    📝 Add arrays example

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 'use strict';
 
-const amIFat = false;
-let somthing;
-console.log(something, amIFat);
+const daysOfWeek = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+
+// Get Item from Array
+console.log(daysOfWeek[4]);
+
+// Add one more day to the array
+daysOfWeek.push('sun');
+
+console.log(daysOfWeek);
