06f93906507af58e598a36c4fae932b1e674f456
pyeonne <pyeonne@gmail.com>
2021-08-16 23:09:58 +0900

    📝 Add CSS in JavaScript example

--- app.js
+++ app.js
@@ -1,40 +1,9 @@
 'use strict';
 
-const h1 = document.querySelector('.hello h1');
+const h1 = document.querySelector('div.hello:first-child h1');
 
 function handleTitleClick() {
-    h1.style.color = 'blue';
-}
-
-function handleMouseEnter() {
-    h1.innerText = 'Moust is hear!';
-}
-
-function handleMouseLeave() {
-    h1.innerText = 'Mouse is gown!';
-}
-
-function handleWindowResize() {
-    document.body.style.backgroundColor = 'tomato';
-}
-
-function handleWindowCopy() {
-    alert('copier!');
-}
-
-function handleWindowOffline() {
-    alert('SOS no WIFI');
-}
-
-function handleWindowOnline() {
-    alert('ALL GOOD');
+    h1.classList.toggle('clicked');
 }
 
 h1.addEventListener('click', handleTitleClick);
-h1.addEventListener('mouseenter', handleMouseEnter);
-h1.addEventListener('mouseleave', handleMouseLeave);
-
-window.addEventListener('resize', handleWindowResize);
-window.addEventListener('copy', handleWindowCopy);
-window.addEventListener('offline', handleWindowOffline);
-window.addEventListener('online', handleWindowOnline);
