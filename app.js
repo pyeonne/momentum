00038a2c906ba7f78201a366abcba108a30de956
pyeonne <pyeonne@gmail.com>
2021-08-16 23:49:17 +0900

    HTML: Login

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
 'use strict';
-
-const h1 = document.querySelector('div.hello:first-child h1');
-
-function handleTitleClick() {
-    h1.classList.toggle('clicked');
-}
-
-h1.addEventListener('click', handleTitleClick);
