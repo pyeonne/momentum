843edfcc35b770ec380ce37ed0983b9cc93a1222
pyeonne <pyeonne@gmail.com>
2021-08-17 15:33:02 +0900

    JS: Loading username

--- app.js
+++ app.js
@@ -5,15 +5,26 @@ const loginInput = document.querySelector('#login-form input');
 const greeting = document.querySelector('#greeting');
 
 const HIDDEN_CLASSNAME = 'hidden';
+const USERNAME_KEY = 'username';
 
 function onLoginSubmit(event) {
     event.preventDefault();
     const username = loginInput.value;
-    localStorage.setItem('username', username);
+    localStorage.setItem(USERNAME_KEY, username);
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    greeting.innerText = 'Hello ' + username;
+    paintGreetings(username);
+}
+
+function paintGreetings(username) {
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener('submit', onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener('submit', onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
