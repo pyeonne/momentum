fbafafaabea0b7100031881057cf9da3e622f6ff
pyeonne <pyeonne@gmail.com>
2021-08-18 15:52:28 +0900

    feat(todo.js): Create function adding todos

--- js/background.js
+++ js/background.js
@@ -2,7 +2,7 @@
 
 const images = ['0.jpg', '1.jpg', '2.jpg'];
 
-const chooseImage = images[Math.round(Math.random() * images.length)];
+const chooseImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement('img');
 
